fb8c4a35b16f74e065ef629040bbde0910117c90
hello <me@stuff.com>
2025-06-05 18:57:16 +0530

    mydiff.c without debugging printfs

--- src/mydiff.c
+++ src/mydiff.c
@@ -27,11 +27,7 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 		mid = (2 * max) - mid;
 	}
 
-	// printf("%d\n", mid);
-
 	v[mid] = 0;
-
-	// printf("%d\n", mid);
 
 	for (int d = 0; d <= max; d++)
 	{
@@ -57,8 +53,6 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 
 			int y = x - k;
 
-			// printf("%d %d %d\n", x, y, k);
-
 			if (y >= 0)
 			{
 				while (x < n && y < m && str1[x] == str2[y])
@@ -75,22 +69,6 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 
 			if (x >= n && y >= m)
 			{
-				// printf("--------\n");
-				// for (int j = 0; j <= 2 * max; j++)
-				// {
-				// 	printf("%d ", v[j]);
-				// }
-
-				// printf("\n");
-
-				// printf("--------%d \n", backtrack.back_d);
-				// for (int j = 0; j < backtrack.back_ind; j++)
-				// {
-				// 	printf("%d ", backtrack.back_arr[j]);
-				// }
-
-				// printf("\n");
-
 				int back_d = backtrack.back_d;
 
 				int cur_k;
@@ -102,11 +80,7 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 				{
 					cur_k = x - y;
 
-					// printf("%d\n", cur_k);
-
 					int base = (a - 1) * (a) / 2 + (a == 1);
-
-					// printf("-----------%d %d\n", base, a);
 
 					for (int l = -a + 1; l <= a - 1; l += 2) // Revert the array to the previous state
 					{
@@ -125,15 +99,6 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 						v[mid] = backtrack.back_arr[0];
 					}
 
-					// printf("\n%d --------\n", a);
-					// for (int j = 0; j <= 2 * max; j++)
-					// {
-					// 	printf("%d ", v[j]);
-					// }
-
-					// printf("\n");
-
-					// printf("%d\n", cur_k + 1 <= a);
 					if (cur_k == -a || (cur_k != a && cur_k + 1 <= a && v[mid + cur_k - 1] < v[mid + cur_k + 1]))
 					{
 						prev_k = cur_k + 1;
@@ -150,54 +115,30 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 						}
 					}
 
-					// printf("--->%d %d\n", prev_k, mid);
-
 					prev_x = v[mid + prev_k];
 					prev_y = prev_x - prev_k;
-
-					// printf("%d %d %d\n", prev_x, prev_k, cur_k);
-
-					// printf("!!!!%d %d\n", prev_x, prev_y);
-					// printf("!!!!%d %d\n", a, prev_k);
-					// printf("!!!!%d %d %d %d\n", v[mid + cur_k - 1], v[mid + cur_k + 1], cur_k, a);
 
 					while (x > prev_x && y > prev_y)
 					{
 						printf("%d %d -> %d %d\n", x - 1, y - 1, x, y);
-
-						// printf("%c\n", str1[x - 1]);
 
 						x--;
 						y--;
 					}
 
 					printf("%d %d -> %d %d\n", prev_x, prev_y, x, y);
+
 					if (x - prev_x == 1)
 					{
 						edit_list->els_arr[edit_list->els_ind++] = (Edit_Node){.ed_ind = x, .ed_type = 'd'};
-						// printf("D%d\n", x);
 					}
 					else if (y - prev_y == 1)
 					{
 						edit_list->els_arr[edit_list->els_ind++] = (Edit_Node){.ed_ind = x, .ed_type = 'i', .ed_val = str2[y - 1]};
-						// printf("I%d%c\n", x, str2[y - 1]);
 					}
-
-					// printf("%d %d\n", cur_k, a);
-
-					// printf("%d %d %c %c\n", x - prev_x, y - prev_y, str1[x - 1], str2[y - 1]);
 
 					x = prev_x;
 					y = prev_y;
-
-					// printf("%d ", back_d);
-
-					// for (int j = 0; j <= max; j++)
-					// {
-					// 	printf("%d ", v[j]);
-					// }
-
-					// printf("\n");
 				}
 
 				while (x > 0 && y > 0)
@@ -208,44 +149,11 @@ int fewest_edits(char *str1, char *str2, Edit_List *edit_list)
 					y--;
 				}
 
-				// if (x != 1 || y != 1) // Check if diagonal from origin
-				// {
-				// 	if (x == 1)
-				// 	{
-				// 		printf("D%d\n", x);
-				// 	}
-				// 	else if (y == 1)
-				// 	{
-				// 		printf("I%d%c\n", x, str2[y - 1]);
-				// 	}
-				// }
-				// printf("0 0 -> %d %d\n", x, y);
-
-				// for (int back_d = 0; back_d <= backtrack.back_d; back_d++)
-				// {
-				// 	int i = back_d * (back_d + 1) / 2;
-
-				// 	for (int x = i; x < i + back_d + 1; x++)
-				// 	{
-				// 		printf("%d ", backtrack.back_arr[x]);
-				// 	}
-				// 	printf("\n");
-				// }
-
 				free(backtrack.back_arr);
 				free(v);
 				return d;
 			}
 		}
-
-		// printf("%d ", d);
-
-		// for (int j = 0; j <= max; j++)
-		// {
-		// 	printf("%d ", v[j]);
-		// }
-
-		// printf("\n");
 
 		backtrack.back_d++;
 	}
